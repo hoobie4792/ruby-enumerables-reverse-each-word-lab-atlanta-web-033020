9a5eb3995123d56f4fd822e18971b8993da9cb32
Brandon Brown <brandonbrown4792@gmail.com>
2020-03-12 18:59:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@ def reverse_each_word(sentence)
   array = sentence.split(' ')
   result = []
   
-  result = array.each do |n|
-    puts n
+  result = array.each do |word|
+    puts word.reverse
   end
   
   return result.join(' ')
